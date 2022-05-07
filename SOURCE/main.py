1e13399b0e19b0237f3164cabf78f5c99fa4b8f1
Stanislav <snorccrafter@gmail.com>
2022-05-07 23:05:52 +0700

    Update main.py

--- SOURCE/main.py
+++ SOURCE/main.py
@@ -13,6 +13,10 @@ class KMSApplication(QtWidgets.QMainWindow, appgui.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self._ui_links()
+
+    def _ui_links(self):
+        pass
 
 
 def main():
